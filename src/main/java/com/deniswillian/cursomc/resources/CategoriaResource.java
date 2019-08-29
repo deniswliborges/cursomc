b274e1608cd1cdc445895ba9c1d9d3c18e8108ff
Denis Willian <deniswliborges@gmail.com>
2019-08-29 02:15:45 -0300

    Rest testado

--- src/main/java/com/deniswillian/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/deniswillian/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,15 @@
 package com.deniswillian.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.deniswillian.cursomc.domain.Categoria;
-
 @RestController
-@RequestMapping(value="/categorias")//EndPoint que irá buscar dados http . ex: http://localhost:8081/categorias
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)// Get para obter Dados | .POST para salvar Dados ou Criar novo recurso .DELETE para deletar
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Esccritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		
-		return lista;
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest esta funcionando!";
 	}
 }
